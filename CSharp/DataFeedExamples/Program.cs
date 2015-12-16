3f099c5ae5cfc5c40082edf4928da55f37662cd4
lotgon <andrei1pazniak@gmail.com>
2015-12-16 16:46:08 +0200

    remove examples

--- CSharp/DataFeedExamples/Program.cs
+++ CSharp/DataFeedExamples/Program.cs
@@ -12,17 +12,7 @@
             var username = Settings1.Default.login;
             var password = Settings1.Default.password;
             
-            //Library.WriteFullDumpOnError(@"D:\full.dmp");
-
-            //var example = new SymbolInfoExample(address, username, password);
             var example = new TicksExample(address, username, password);
-            //var example = new BarsHistoryExample(address, username, password);
-            //var example = new StorageTicksHistoryExample(address, username, password);
-            //var example = new StorageTicksRangeIteratorHistoryExample(address, username, password);
-            //var example = new StorageBarsHistoryExample(address, username, password);
-            //var example = new StorageUpdatingExample(address, username, password);
-            // var example = new ImportQuotesExample();
-            //var example = new StorageMultithreadingExample(address, username, password);
 
             using (example)
             {
